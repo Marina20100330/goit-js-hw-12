a5519aa16f6f2cbc7cfde773a2406b9cf6a78335
Marina20100330 <marinamaksimovskaa3@gmail.com>
2025-06-30 10:48:40 +0000

    Deploying to gh-pages from @ Marina20100330/goit-js-hw-12@6a554634a89ff1b71ee997343bc550b90f920752 🚀

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import{a as w,S,i as a}from"./assets/vendor-DxEWe7lX.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))c(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const s of o.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&c(s)}).observe(document,{childList:!0,subtree:!0});function n(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerPolicy&&(o.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?o.credentials="include":e.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function c(e){if(e.ep)return;e.ep=!0;const o=n(e);fetch(e.href,o)}})();const M="50349808-906d80db7093834bea09f6975",q="https://pixabay.com/api/";async function u(r,t=1){return(await w.get(q,{params:{key:M,q:r,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:15,page:t}})).data}const m=document.querySelector(".gallery"),f=document.querySelector(".loader"),p=document.querySelector(".load-more"),B=new S(".gallery a");function h(r){const t=r.map(({webformatURL:n,largeImageURL:c,tags:e,likes:o,views:s,comments:v,downloads:b})=>`
+import{a as w,S as M,i as s}from"./assets/vendor-DxEWe7lX.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const i of o.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&l(i)}).observe(document,{childList:!0,subtree:!0});function n(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerPolicy&&(o.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?o.credentials="include":e.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function l(e){if(e.ep)return;e.ep=!0;const o=n(e);fetch(e.href,o)}})();const P="50349808-906d80db7093834bea09f6975",B="https://pixabay.com/api/";async function f(r,t=1){return(await w.get(B,{params:{key:P,q:r,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:15,page:t}})).data}const y=document.querySelector(".gallery"),h=document.querySelector(".loader"),p=document.querySelector(".load-more"),$=new M(".gallery a");function g(r){const t=r.map(({webformatURL:n,largeImageURL:l,tags:e,likes:o,views:i,comments:S,downloads:q})=>`
     <li class="gallery-item">
-      <a href="${c}">
+      <a href="${l}">
         <img class="img" src="${n}" alt="${e}" />
       </a>
     <div class="info">
@@ -10,17 +10,17 @@ import{a as w,S,i as a}from"./assets/vendor-DxEWe7lX.js";(function(){const t=doc
   </div>
   <div class="info-item">
     <p class="label">Views:</p>
-    <p class="value">${s}</p>
+    <p class="value">${i}</p>
   </div>
   <div class="info-item">
     <p class="label">Comments:</p>
-    <p class="value">${v}</p>
+    <p class="value">${S}</p>
   </div>
   <div class="info-item">
     <p class="label">Downloads:</p>
-    <p class="value">${b}</p>
+    <p class="value">${q}</p>
   </div>
 </div>
 </li>
-  `).join("");m.insertAdjacentHTML("beforeend",t),B.refresh()}function $(){m.innerHTML=""}function E(){f.classList.remove("is-hidden")}function P(){f.classList.add("is-hidden")}function y(){p.classList.remove("hidden")}function g(){p.classList.add("hidden")}const L=document.querySelector(".load-more-loader");function x(){L.classList.remove("is-hidden")}function O(){L.classList.add("is-hidden")}const H=document.querySelector(".form"),T=document.querySelector(".load-more");let l="",i=1,d=0;H.addEventListener("submit",A);T.addEventListener("click",N);async function A(r){if(r.preventDefault(),l=r.currentTarget.elements["search-text"].value.trim(),!l){a.warning({message:"Please enter a search query."});return}i=1,$(),g(),E();try{const t=await u(l,i);if(d=t.totalHits,t.hits.length===0){a.error({message:"No images found. Try again!"});return}h(t.hits),d>15&&y()}catch{a.error({message:"Error fetching data"})}finally{P()}}async function N(){i+=1,g(),x();try{const r=await u(l,i);h(r.hits),_();const t=Math.ceil(d/15);i>=t?a.info({message:"We're sorry, but you've reached the end of search results."}):y()}catch{a.error({message:"Error loading more images"})}finally{O()}}function _(){const{height:r}=document.querySelector(".gallery-item").getBoundingClientRect();window.scrollBy({top:r*2,behavior:"smooth"})}
+  `).join("");y.insertAdjacentHTML("beforeend",t),$.refresh()}function E(){y.innerHTML=""}function v(){h.classList.remove("is-hidden")}function L(){h.classList.add("is-hidden")}function u(){p.classList.remove("hidden")}function m(){p.classList.add("hidden")}document.querySelector(".load-more-loader");const b=document.querySelector(".form"),x=document.querySelector(".load-more");let c="",a=1,d=0;b.addEventListener("submit",O);x.addEventListener("click",H);async function O(r){if(r.preventDefault(),c=r.currentTarget.elements["search-text"].value.trim(),!c){s.warning({message:"Please enter a search query."});return}a=1,E(),m(),v();try{const t=await f(c,a);if(d=t.totalHits,t.hits.length===0){s.error({message:"Sorry, there are no images matching your search query. Please try again!"});return}g(t.hits),d<=15?s.info({message:"We're sorry, but you've reached the end of search results."}):u()}catch{s.error({message:"Error fetching data. Please try again later."})}finally{L(),b.reset()}}async function H(){a+=1,m(),v();try{const r=await f(c,a);g(r.hits),A();const t=Math.ceil(d/15);a>=t?(s.info({message:"We're sorry, but you've reached the end of search results."}),m()):u()}catch{s.error({message:"Error loading more images. Please try again later."}),a<Math.ceil(d/15)&&u()}finally{L()}}function A(){const r=document.querySelector(".gallery-item");if(r){const{height:t}=r.getBoundingClientRect();window.scrollBy({top:t*2,behavior:"smooth"})}}
 //# sourceMappingURL=index.js.map
